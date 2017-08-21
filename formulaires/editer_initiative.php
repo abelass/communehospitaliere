7539e22851eaa633ebc23375c21ca8defe199220
abelass <rainer@websimple.be>
2017-08-21 11:33:18 +0200

    Plus obligatoires
    "Nom de la personne de contact" et  "Prénom de la personne de contact"

--- formulaires/editer_initiative.php
+++ formulaires/editer_initiative.php
@@ -115,7 +115,7 @@ function formulaires_editer_initiative_verifier_dist($id_initiative = 'new', $re
 	}
 
 
-	$erreurs += formulaires_editer_objet_verifier('initiative', $id_initiative, array('id_gis', 'type_initiative', 'nom_contact', 'prenom_contact', 'email', 'telephone'));
+	$erreurs += formulaires_editer_objet_verifier('initiative', $id_initiative, array('id_gis', 'type_initiative', 'email', 'telephone'));
 
 	// Ne pas réutiliser une commune.
 	if ($id_gis = _request('id_gis')) {
